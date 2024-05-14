63bbcb61e9fd1e9f11faecb18f23af248d60187e
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-05-14 19:39:25 -0400

    added price change css effect

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,18 +1,62 @@
+'use client'
 import '../styles/quoteCard.css'
+import { useState, useEffect } from 'react'
+
 
 const QuoteCard = (props) => {
-
+    
     const title = props.title
-    const price = props.price
-    const delta = props.variation
+    const [price, setPrice] = useState(props.price)
+    const [priceStyle, setPriceStyle] = useState({backgroundColor: 'rgba(255, 255, 255, 0)'})
+    const [delta, setDelta] = useState(props.variation)
+    const [deltaStyle, setDeltaStyle] = useState({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0)`, color: 'white'})
 
+    const debug = false
+
+    useEffect(()=>{
+        if(debug){
+            setInterval(()=>{
+                setPrice(price + Math.random()*100)
+                setDelta(delta + Math.random()*10 - 5)
+            },5000+Math.random()*2000)
+    
+        }
+    },[])
+
+    useEffect(()=>{
+        setPriceStyle({backgroundColor:'rgba(255, 255, 255, 0.8)'})
+        setTimeout(()=>{
+            setPriceStyle({backgroundColor:'rgba(255, 255, 255, 0)'})
+        },300)
+    },[price])
+
+    useEffect(()=>{
+        if(delta < 0){
+            setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0.8)`, color: '#00FF00'})
+            setTimeout(()=>{
+                setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0)`, color: '#00FF00'})
+            },500)
+        }
+        else if(delta > 0){
+            setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0.8)`, color: 'red'})
+            setTimeout(()=>{
+                setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0)`, color: 'red'})
+            },500)
+        }
+        else {
+            setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0.8)`, color: 'white'})
+            setTimeout(()=>{
+                setDeltaStyle({backgroundColor:`rgba(${delta < 0? 0 : 255}, ${delta >= 0 ? 0 : 255}, 0, 0)`, color: 'white'})
+            },500)
+        }
+    },[delta])
 
     return(
         <>
         <div className="quoteCard">
             <h1 className="quoteTitle">{title.toUpperCase()}</h1>
-            <h2 className="quotePrice">$ {price.toFixed(2)}</h2>
-            <p style={{'color': delta > 0 ? 'red' : delta < 0 ? 'green' : 'white' }}>{delta > 0 ? '▲' : '▼'} {delta.toFixed(2)} %</p>
+            <h2 className="quotePrice" style={priceStyle}>$ {price.toFixed(2)}</h2>
+            <p style={deltaStyle}>{delta > 0 ? '▲' : '▼'} {delta.toFixed(2)} %</p>
         </div>
         </>
     )

bbbe9fc30455bc223140cde5ad74260d87241011
LivioDR <liviodavidreinoso@yahoo.com.ar>
2025-04-23 14:10:00 -0400

    fix(appConfig): adjust refresh time logic for dynamic behavior based on time of day

--- src/app/config/appConfig.js
+++ src/app/config/appConfig.js
@@ -1,6 +1,6 @@
 const appConfig = {
     debug: false,
     autoRefresh: true,
-    refreshTimeMs: 15000,
+    refreshTimeMs: new Date().getHours() > 17 || new Date().getHours() < 8 ? 15000 : 5000,
 }
 export default appConfig
